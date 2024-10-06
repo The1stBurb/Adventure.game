5023d87b2d2d2da238bb57230aaad97ac95722ee
The1stBurb <150498673+The1stBurb@users.noreply.github.com>
2024-10-06 02:21:21 +0000

    asfasf

--- Adventure.game/adventureSupp.py
+++ Adventure.game/adventureSupp.py
@@ -4,4 +4,6 @@ class bg:
         self.nm,self.hp,self.atk=name,hp,atk
     def __str__(self):
         return f"{self.nm}:\n  HP: {self.hp}\n  Atk: {self.atk}\n"
-bads=[bg("squirrle",5,1)]
+bads=[bg("squirrle",5,1),bg("Oger",10,5),bg("Trul",10,3),bg("Rok",15,3),bg("Duckinz",7,2),
+      bg("Burbostym",20,5),bg("Kajs Kaf",3,1),bg("Feral dohg",5,3),bg("Asplig",10,2),bg("Duklahf",25,2),
+      bg("Afurn",12,1.5),bg("Credlin",5,7),bg("Bohglin",6,6),bg("Eiatlin",7,5),bg("Afurlin",9,3),]#bg("",,),
