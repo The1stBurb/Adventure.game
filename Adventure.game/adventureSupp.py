6088a373dc62ae9ef4a8560b11720778714a4a9b
Etcetera_ <150498673+The1stBurb@users.noreply.github.com>
2024-10-07 11:44:58 -0600

    Update adventureSupp.py
    fd kj f fds kjgfdskjgfds

--- Adventure.game/adventureSupp.py
+++ Adventure.game/adventureSupp.py
@@ -1,4 +1,3 @@
-#add the craft anbd stuff dicts
 class bg:
     def __init__(self,name,hp,atk):
         self.nm,self.hp,self.atk=name,hp,atk
@@ -7,3 +6,32 @@ class bg:
 bads=[bg("squirrle",5,1),bg("Oger",10,5),bg("Trul",10,3),bg("Rok",15,3),bg("Duckinz",7,2),
       bg("Burbostym",20,5),bg("Kajs Kaf",3,1),bg("Feral dohg",5,3),bg("Asplig",10,2),bg("Duklahf",25,2),
       bg("Afurnlin",10.5,1.5),bg("Credlin",5,7),bg("Bohglin",6,6),bg("Eiatlin",7,5),bg("Afurlin",9,3),]#bg("",,),
+eff={
+    "":" have no effects!",
+    "hyd":" got hydrated!",
+    "fire":"r on fire!!",
+    "pois":"v'e been poisoned!",
+    "haluc":"r halucinating!",
+    "unc":"r unconscious!",
+    "wind":"r winded!",
+    "conc":" have a concussion",# can lead to "unc"
+    "fell":" fell",
+}
+wepon={
+    "axe":{"blk":2,"atk":4,"desc":"an axe, aight?","eff":[],},
+    "pickaxe":{"blk":1,"atk":2,"desc":"not very good wepon","eff":[],},
+    "sword":{"blk":2,"atk":5,"desc":"pokey staff thing","eff":[],},
+    "swrod":{"blk":4,"atk":5,"desc":"better pokey staff","eff":[],},
+    "sword of fire":{"blk":2,"atk":6,"desc":"a pokey staff that makes fire","eff":["fire"],},
+    "sword of water":{"blk":4,"atk":5,"desc":"a pokey staff that makes water","eff":["hyd"],},
+    "sword of air":{"blk":1,"atk":9,"desc":"a pokey staff that makes air","eff":["wind"],},
+    "sword of earth":{"blk":7,"atk":4,"desc":"a pokey staff that makes earth","eff":["fell"],},
+    "club":{"blk":4,"atk":3,"desc":"smackey staff","eff":["conc","wind"],},
+    "great club":{"blk":4,"atk":5,"desc":"greater smackey staff","eff":["conc","wind","fell"],},
+    "great sword":{"blk":3,"atk":8,"desc":"great pokey staff","eff":[],},
+    "greater sword":{"blk":2,"atk":10,"desc":"greater pokey staff","eff":[],},
+    "great swrod":{"blk":6,"atk":9,"desc":"grear better pokey staff","eff":[],},
+    "greater swrod":{"blk":5,"atk":12,"desc":"greater better pokey staff","eff":[],},
+    "staff":{"blk":5,"atk":3,"desc":"a staff","eff":["fell","wind"],},
+    "staf":{"blk":5,"atk":5,"desc":"a staf","eff":["fell","conc","wind"],},
+}#"":{"blk":,"atk":,"desc":"","eff":[],},
