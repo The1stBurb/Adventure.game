49514183c0814a1cda71c418dc9b82f85a09a599
Etcetera_ <150498673+The1stBurb@users.noreply.github.com>
2024-10-25 21:39:14 -0600

    Update adventureBuilds.py
    renovations are coming!

--- Adventure.game/adventureBuilds.py
+++ Adventure.game/adventureBuilds.py
@@ -5,9 +5,12 @@ from ast import literal_eval
 # from adventureText import tprint
 # tprint("I am mehher at coding sooo")
 fr="~!@#$%^&*()_+QWERTYUIOP{|}ASDFGHJKL:\"ZXCVBNM<>?`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./ "
-to="L47U1h'Q\"|C2*8j/td\\-kulPGZa.0K<b;)q$%w?MBYg#3Fm}JyH[pIDo&(5sN`6TES_e]x!v ArzW9ni=c@X:~,{+f>^OVR"#sample(list(fr),len(fr))
+to="L47U1h'Q\"|C2*8j/td\\-kulPGZa.0K<b;)q$%w?MBYg#3Fm}JyH[pIDo&(5sN`6TES_e]x!v ArzW9ni=c@X:~,{+f>^OVR"#⧅ᴓ⎋ඞ®⁂₡ℐℵς⅟↭⟲⥲⥿∳≉⊎⟁⟫⦕⦫⦜⧨⩥⫸⌘⌂⌬⎃⍲⏟␥┟╦◷🟢🞜🟀♛♗⛟❖➼⢻⡰⭍⮍⮷𝅀𝆔🃇🃬🍁🖍🛪🖯🕪🕱🏎🂊🁚𝆸𝄑sldǝɯdɥ𝓊𝓃𝒻𝒾𝓇𝑒𝓈เ ฬคภՇŦ๏๔ʜꜱʟᴋꜰᴏᴘᴀ"#sample(list(fr),🁚len(fr))
 # print("".join(to),len(fr),len(to))
+# print(len(to),len(fr))
 # to=""
+
+#convert save text
 def conv(*txt):
     txt=str(txt)
     # print(txt)
@@ -18,6 +21,7 @@ def conv(*txt):
         else:
             nt+=i
     return nt
+#deconvert save text
 def decon(txt):
     txt=str(txt)
     print(txt)
@@ -28,20 +32,14 @@ def decon(txt):
         else:
             nt+=i
     return nt
+# save it!
 def sv(*txt):
     r=conv(*txt)
     print("Here's your save code!:",r)
+    return r
+##unsave it
 def unsv(txt):
     r=literal_eval(decon(txt))
+    # with open("save.txt","w")as sve:
+    #     sve.write(r)
     return r
-# sv("qwerty")
-# with open("save.txt","a")as sve:
-#     # sve.write(r)
-#     sve.write("oops")
-# sve=open("save.txt","a")
-# sve.write("akjga")
-# sve.close()
-# print(conv("qwerty"))
-# b=conv(p,8.2346)
-# print(b)
-# print(decon(b))
