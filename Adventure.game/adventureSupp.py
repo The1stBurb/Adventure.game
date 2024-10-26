54cebb08b39fbfb8b630f9a3426baf186023f583
Etcetera_ <150498673+The1stBurb@users.noreply.github.com>
2024-10-25 21:39:41 -0600

    Update adventureSupp.py
    renovations going on rn

--- Adventure.game/adventureSupp.py
+++ Adventure.game/adventureSupp.py
@@ -1,4 +1,6 @@
 from random import choice,randint
+from time import sleep
+#diff wepons
 wepon={
     #L1
     "":{"blk":0,"atk":0,"desc":"not a wepaon","eff":[],},
@@ -10,11 +12,13 @@ wepon={
     "swrod":{"blk":4,"atk":5,"desc":"better pokey staff","eff":[],},
     "club":{"blk":4,"atk":3,"desc":"smackey staff","eff":["conc","wind"],},
     "staff":{"blk":5,"atk":3,"desc":"a staff","eff":["fell","wind"],},
+    "sheild":{"blk":5,"atk":0.1,"desc":"it sheilds you","eff":[],},
 
     #L3
     "great club":{"blk":4,"atk":5,"desc":"greater smackey staff","eff":["conc","wind","fell"],},
     "great sword":{"blk":3,"atk":8,"desc":"great pokey staff","eff":[],},
     "great swrod":{"blk":6,"atk":9,"desc":"grear better pokey staff","eff":[],},
+    "great sheild":{"blk":10,"atk":2,"desc":"it sheilds you greater","eff":[],},
     
     "greater sword":{"blk":2,"atk":10,"desc":"greater pokey staff","eff":[],},
     "greater swrod":{"blk":5,"atk":12,"desc":"greater better pokey staff","eff":[],},
@@ -30,13 +34,46 @@ wepon={
     "shardium blade":{"blk":13,"atk":12,"desc":"a blade, made of the mythic metal \"shardinum\"","eff":["pois","pois","pois","conc"],},
     "farthium staff":{"blk":20,"atk":10,"desc":"an ancient staff, made of a lost metal: \"farthide\"","eff":["wind","pois","pois","unc","unc","wind","conc","conc"],},
     "doomstaff":{"blk":15,"atk":15,"desc":"bringer of doom","eff":["dyi","conc","conc","wind","unc","unc"],},
+    "scrak's blade":{"blk":5,"atk":20,"desc":"blade wielded by scrak","eff":["unc","slw","fell"],},
+    "demon blade":{"blk":20,"atk":25,"desc":"a demon forged blade","eff":["fire","slw","haluc"],},
+    "ancient one's sheild":{"blk":30,"atk":5,"desc":"the ancient ones sheild","eff":[""],},
+    "dragon sheild":{"blk":50,"atk":-2,"desc":"a legendary sheild, used in the draconic wars","eff":["fire","slw","conc","haluc"],},
+    # "burvarian chestplate":{"blk":,"atk":,"desc":"","eff":[],},
+    "axe of cleaving":{"blk":5,"atk":30,"desc":"a killers axe","eff":["wind","dyi"],},
+    "travelers staff":{"blk":30,"atk":20,"desc":"purportedly used by the traveler","eff":["fell","conc","unc","wind"],},
+    "poker stick":{"blk":0,"atk":30,"desc":"idk why its so good but","eff":["wind"],},
+    "calculater":{"blk":30,"atk":50,"desc":"wild bear place legacy item","eff":["conc"],},
+    "liver":{"blk":50,"atk":40,"desc":"throwback to better times","eff":["dyi"],},
+    "cerebri scissura":{"blk":10,"atk":20,"desc":"brain render","eff":["haluc","conc","unc"],},
+    "aether staff":{"blk":30,"atk":30,"desc":"fused with aether its really cool","eff":["blnd","conc"],},
+    "torch":{"blk":5,"atk":5,"desc":"more legacy","eff":["fire","fire","fire","fire","conc"],},
+    "the burbian blade":{"blk":50,"atk":50,"desc":"you wont get it i know, but wieled by the protectors of the burbian peopls","eff":["hyd","fire","pois","haluc","conc","unc","wind","conc","fell","dyi","slw","res","blnd"],},
 }#"":{"blk":,"atk":,"desc":"","eff":[],},
 #L1->not really wepon,L2->basic wepon,L3->better wepon,L4->really good wepon,L5->mythic
-p=[0,0,[["Handbook",1],["seed",100]],{},100,1,0,{"helmet":["",0],"chestplate":["",0],"right_hand":["",0],"left_hand":["",0],"pants":["",0],"boots":["",0],},{"mon1":0,"mon2":0,"mon3":0,"mon4":randint(5,10),}]
-#0-x map, 1-y map, 2-inv[name, amnt],3-effects,4-hp,5-atk,6-hunger,7-equiped
+#you
+p=[0,0,[["Handbook",1],["tomer",5]],{"unc":0},100,1,0,{"helmet":["",0],"chestplate":["",0],"right_hand":["",0],"left_hand":["",0],"pants":["",0],"boots":["",0],},{"mon1":0,"mon2":0,"mon3":0,"mon4":randint(5,10),},[0,0],
+{"knwn":{},"lkd":{
+    "fireball":{"dmg":5,"eff":["fire"],"hel":0,"lv":5,"xpc":0},
+    "heal":{"dmg":1,"eff":[],"hel":5,"lv":6,"xpc":1},
+    "trippy":{"dmg":2,"eff":["fall","haluc","wind"],"hel":0.01,"lv":10,"xpc":2},
+    "nerd":{"dmg":3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651,"eff":["haluc"],"hel":-1,"lv":8,"xpc":0.5},
+    "inner_stab":{"dmg":7,"eff":["dyi","wind"],"hel":0,"lv":10,"xpc":3},
+    "smack":{"dmg":2,"eff":["conc"],"hel":-1,"lv":9,"xpc":0},
+    "poison":{"dmg":2,"hel":0,"eff":["pois"],"lv":12,"xpc":5},
+    "freeze":{"dmg":3,"hel":-0.1,"eff":["slw"],"lv":6,"xpc":0},
+    "resist":{"dmg":1,"hel":5,"eff":[],"lv":15,"xpc":10},#"res" for i in range(5)
+    "finagle":{"dmg":1,"hel":2,"eff":["codep[8][\"mon4\"]+=randint(2,4)"],"lv":10,"xpc":5},
+    "tp":{"dmg":0,"hel":-5,"eff":["codep[0]=randint(0,len(mp)\np[1]=randint(0,len(mp[0])\nfg=False"],"lv":100,"xpc":10**10},
+    # "":{"dmg":,"hel":,"eff":[],"lv":,"xpc":0},
+    # "":{"dmg":,"hel":,"eff":[],"lv":,"xpc":0},
+}#"":{"dmg":,"hel":,"eff":[],"lv":,"xpc":},
+},#spells
+0,0]
+#0-x map, 1-y map, 2-inv[name, amnt],3-effects,4-hp,5-atk,6-hunger,7-equiped,8-mon,9-xp,10-spells,11-movability,12-dodge amount
+#textify your effects
 eff={
     "":" have no effects!",
-    "hyd":" got hydrated!",
+    "hyd":" got hydrated!",# can unfire you
     "fire":"r on fire!!",
     "pois":"v'e been poisoned!",
     "haluc":"r halucinating!",
@@ -45,41 +82,64 @@ eff={
     "conc":" have a concussion",# can lead to "unc"
     "fell":" fell",
     "dyi":"r dying",
+    "slw":"'ve been slowed down",
+    "res":"r resistant",
+    "blnd":" can't see"
 }
+#run effects
 def effd():
     global p
     for i in p[3]:
         # print(i)
         if p[3][i]>0:
-            if randint(0,2)==0:
+            if i in ["hyd","unc","fell","dyi"]:
                 p[3][i]-=1
-        if randint(0,1)==0:
-            match i:
-                case "hyd":
-                    p[6]+=0.1
-                case "fire":
-                    p[4]-=4
-                case "pois":
-                    p[4]-=2
-                case "haluc":
-                    p[4]+=randint(-1,1)/10
-                    p[6]+=randint(-1,1)/10
-                case "unc":
-                    pass
-                case "conc":
-                    if randint(0,2)==0:
-                        p[3][i]-=1
-                        p[3]["haluc"]+=1
-                case "fell":
-                    p[6]-=5
-                    if randint(0,1)==0:
-                        p[3][i]-=1
-                case "dyi":
-                    p[4]-=(p[4]+10)/2
+            elif randint(0,2)==0 and not i in ["conc"]:
+                p[3][i]-=1
+            # if randint(0,1)==0:
+            if i=="hyd":
+                p[6]+=0.1
+                if p[3]["fire"]>0 and randint(0,2)==0:
+                    p[3]["fire"]-=1
+                # p[3][i]-=1
+            elif i=="fire":
+                p[4]-=4
+            elif i=="pois":
+                p[4]-=2
+            elif i=="haluc":
+                p[4]+=randint(-1,1)/10
+                p[6]+=randint(-1,1)/10
+                if randint(0,2)==0:
+                    p[11]+=1
+            if i=="unc":
+                s=randint(5,10)
+                # for i in range(s):
+                #     print(f"You will be unconcious for {s-i} more seconds!")
+                #     sleep(1)
+                p[11]+=s
+            elif i=="conc":
+                p[11]+=raandint(0,2)
+                if randint(0,2)==0:
+                    p[3][i]-=1
+                    if randint(0,5)!=0:
+                        p[3][choice(["haluc","unc"])]+=1
+                    
+            elif i=="fell":
+                p[6]-=5
+                p[9][0]-=min(p[9][0],5)+randint(-1,2)
+                s=randint(2,5)
+                # for i in range(s):
+                #     print(f"Your getting up from your fall! You will be up in {s-i} seconds!")
+                #     sleep(1)
+                p[11]+=s
+            elif i=="dyi":
+                p[4]-=(p[4]+10)/2
         p[6]=min(13,max(0,p[6]))
         p[4]=max(1,p[4])
+#capitoloze
 def cap(txt):
     return txt[0].upper()+txt[1:]
+#bas giy class
 class bg:
     def __init__(self,name,hp,atk,wpn=[""],b=1):
         # print(wpn)
@@ -89,15 +149,19 @@ class bg:
             if wpn=="sword of":
                 wpn=wpn+choice([" fire"," water"," air"," earth"])
         self.nm,self.hp,self.atk,self.wpn=name,hp,atk,[wepon[wpn],wpn]if not isinstance(wpn,list)else wpn
+        self.blk=0
         self.mon={"mon1":0,"mon2":0,"mon3":0,"mon4":randint(3,10),}#max(0,randint(-3,2)),max(0,randint(-1,3)),randint(0,5),0
         self.inv={}
+        self.mhp=self.hp
     def sw(self):
         if self.wpn[1]!="":
-            return f"Weapon: {cap(self.wpn[1])}\n  BLK: {self.wpn[0]["blk"]}\n  ATK: {self.wpn[0]["atk"]}\n"
+            return "Weapon: "+cap(self.wpn[1])+"\n  BLK: "+str(self.wpn[0]["blk"])+"\n  ATK: "+str(self.wpn[0]["atk"])+"\n"
         return ""
     def __str__(self):
-        return f"{self.nm}:\n  HP: {self.hp}\n  Atk: {self.atk}"
-bads=[#bg("squirrle",5,1),
+        return self.nm+":\n  HP: "+str(self.hp)+"\n  Atk: "+str(self.atk)
+#differwnt bad guys
+bads=[
+      #bg("squirrle",5,1),
       bg("Oger",10,5,["club","club","club","great club"],0),
       bg("Trul",10,3,["club","club","club","great club","sword"],0),
       bg("Rok",15,3,["club"],0),
@@ -105,7 +169,7 @@ bads=[#bg("squirrle",5,1),
       bg("Burbostym",20,5,["staff","staff","staf","sword","staff","staff","swrod"],0),
       bg("Kajs Kaf",3,1,["staff","staf","sword","swrod","axe","club","great sword","great club","sword of"],0),
       bg("Feral dohg",5,3,[],0),
-      bg("Asplig",10,2,["sword of","sword of","sword","swrod","great club","grear sword","staff","staf"],0),
+      bg("Asplig",10,2,["sword of","sword of","sword","swrod","great club","great sword","staff","staf"],0),
       bg("Duklahf",25,2,["sword of","sword","swrod","great sword","greater sword","great swrod","greater swrod","staf"],0),
 
       bg("Afurnlin",10.5,1.5,["sword of","sword","swrod","great sword","greater sword","great swrod","greater swrod","staf","club","great club","sword of","sword of"],0),
@@ -113,15 +177,12 @@ bads=[#bg("squirrle",5,1),
       bg("Bohglin",6,6,["sword of","sword","swrod","great sword","greater sword","great swrod","greater swrod","staf","club","great club","sword of","sword of"],0),
       bg("Eiatlin",7,5,["sword of","sword","swrod","great sword","greater sword","great swrod","greater swrod","staf","club","great club","sword of","sword of"],0),
       bg("Afurlin",9,3,["sword of","sword","swrod","great sword","greater sword","great swrod","greater swrod","staf","club","great club","sword of","sword of"],0),
-    ]#bg("",,[],0),
+    ]#bg("",,,[],0),
+#magic spels
 spls={
-    "fireball":{"dmg":5,"eff":["fire"],"hel":0},
-    "heal":{"dmg":1,"eff":[],"hel":5},
-    "trippy":{"dmg":2,"eff":["fall","haluc","wind"],"hel":0.01},
-    "nerd":{"dmg":3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651,"eff":["haluc"],"hel":-1},
-    "inner_stab":{"dmg":5,"eff":["dyi","wind"],"hel":0},
-    "smack":{"dmg":3,"eff":["conc"],"hel":-1},
+    
 }#"":{"dmg":,"eff":[],"hel":},
+#money changer
 def moner(tp,tp2,am):
     con=1
     #m1=3x m2
@@ -157,6 +218,7 @@ def moner(tp,tp2,am):
             con=1/5
     # print(am,con,round(am*con))
     return am-round(round(am*con)/con),round(am*con)
+#twxtify money for shop
 def demon(mon):
     m={"mon1":0,"mon2":0,"mon3":0,"mon4":0,}
     for i in [["4","1"],["4","2"],["4","3"]]:
